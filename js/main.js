b782741886933189aa41bd7f869a036d19d9c363
lattila1 <73441214+lattila1@users.noreply.github.com>
2020-12-17 11:55:42 +0100

    Add preloader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 function pageLoad () {
+  const root = document.getElementById('root');
+  const content = root.innerHTML;
+  root.innerHTML = '';
 
-//  document.getElementById("root").insertAdjacentHTML('afterbegin', `
-
+  root.insertAdjacentHTML('beforeend', `
+    <div id="preloader">
+      <img src="../img/preloader.png" alt="">
+    </div>
+  `);
+  setTimeout(() => {
+    root.innerHTML = content;
+  }, 3000);
 }
 
 window.addEventListener('load', pageLoad);
--- js/page-loading.js
+++
@@ -1,6 +0,0 @@
-function pageLoad () {
-
-
-}
-
-window.addEventListener('load', pageLoad);
